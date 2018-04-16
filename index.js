a356bdf0f8c1718949d3a5764d8a3dd5b6bc7fe0
Leonard Martin <leonard.martin@gmail.com>
2018-04-16 11:47:22 +0100

    Add debugging

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', (req, res, next) => {
 
+  console.log('Received incoming deploy hook');
+  console.log(req.body);
+
+  const text = `App: ${req.body.app} was deployed to ${req.body.url}`;
+
   const body = JSON.stringify({
     payload: {
       channel: process.env.SLACK_CHANNEL || '#general',
-      text: `App: ${req.body.app} was deployed to ${req.body.url}`
+      text
     }
   });
 
@@ -18,8 +23,12 @@ app.post('/', (req, res, next) => {
     'Content-type': 'application/x-www-form-urlencoded'
   };
 
+  console.log('Sending payload to slack');
+  console.log(body);
+
   fetch.post(process.env.SLACK_URL, { body, headers }).text
   .then(text => {
+    console.log('Sent successfully');
     res.send('ok');
   })
   .catch(next);
